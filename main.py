3b0891b35b7a63301e619674700bca6a119f5f8e
Osama Said Al Zakwani <Osama.Zakwani@gmail.com>
2025-12-18 09:13:54 +0400

    done with the second feature

--- main.py
+++ main.py
@@ -6,14 +6,16 @@ import os
 app = FastAPI()
 
 
-SWAPI_BASE_URL = "https://swapi.tech/api/people"
+SWAPI_TECH_URL = "https://swapi.tech/api/people"
+SWAPI_HOMEWORLD_URL = "https://swapi.info/api/people"
 
 file_path = "./data/swapi_char.csv"
+file_path_details = "./data/swapi_details.csv"
 
 @app.get("/characters")
 def get_all_char():
     try:
-        res = requests.get(SWAPI_BASE_URL)
+        res = requests.get(SWAPI_TECH_URL)
         data = res.json()
         results = data["results"]
 
@@ -39,6 +41,35 @@ def get_all_char():
         return results
     except Exception as e:
         return {"error": str(e)}
+    
+
+
+@app.get("/homeworld")
+def get_all_homeworld():
+    try:
+        res = requests.get(SWAPI_HOMEWORLD_URL)
+        data = res.json()
+        result = data[:29]
+
+        if not os.path.exists(file_path_details):
+            with open(file_path_details, "a") as csv_file:
+                csv_file.write("name,height,mass,hair_color,skin_color,eye_color,birth_year,gender,homeworld,films\n")
+                for details in result:
+                    name = details["name"]
+                    height = details["height"]
+                    mass = details["mass"]
+                    hair_color = details["hair_color"]
+                    skin_color = details["skin_color"]
+                    eye_color = details["eye_color"]
+                    birth_year = details["birth_year"]
+                    gender = details["gender"]
+                    homeworld = details["homeworld"]
+                    films = details["films"]
+                    csv_file.write(f"{name},{height},{mass},{hair_color},{skin_color},{eye_color},{birth_year},{gender},{homeworld},{films}\n")
+
+    
+    except Exception:
+        print("Error on fetching data from secound database")
 
 
 
